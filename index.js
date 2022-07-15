240bb6f942be98dbfd753bfb784fa0688e91eeb0
Yanek K <yanek.dev@gmail.com>
2022-07-14 23:48:08 -0400

    uibuild

--- index.js
+++ index.js
@@ -98,20 +98,18 @@ app.get("/api/persons/:id", (request, response, next) => {
 
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "Name or Number is missing",
-    });
-  }
+
   const person = new Person({
     name: body.name,
     number: body.number,
   });
+
   if (duplicatePerson(person)) {
     return response.status(400).json({
       error: "Name must be unique",
     });
   }
+
   person
     .save()
     .then((newPerson) => {
@@ -129,14 +127,13 @@ app.delete("/api/persons/:id", (request, response, next) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -153,6 +150,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "Malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "AxiosError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
