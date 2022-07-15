7a8308531b065daac313f7859130d819bcf3f8f8
Yanek K <yanek.dev@gmail.com>
2022-07-15 12:24:31 -0400

    Ex 3.20 Complete

--- models/person.js
+++ models/person.js
@@ -21,7 +21,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 9,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
     required: true,
   },
   id: String,
